28a5a84c34eeabfb3f756550b9ab4b74d874f62e
JuliustheCreator <Jrbroomfield1@gmail.com>
2023-09-09 13:59:35 -0400

    updated src files

--- backend/VirtualEyeExam/eye_exam/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EyeExamConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "eye_exam"
--- backend/VirtualEyeExam/speech_to_text/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SpeechToTextConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "speech_to_text"
