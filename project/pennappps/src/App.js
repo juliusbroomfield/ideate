1e2258355fac11d4812abfa61fa458587481add1
Ammaar <ammaar1anjum@gmail.com>
2023-09-09 16:42:07 -0500

    installed react router hooks and doms and configured the main menifold correctly

--- project/pennappps/src/App.js
+++ project/pennappps/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react'; // Often necessary to import React, depending on your React version and setup.
 import './App.css';
+import LoginPage from "./components/LoginPage";
+
+// Missing imports for react-router-dom
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ project/pennappps/src/components/LoginPage.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
+
+export default function SignInPage() {
+  const [username, setUsername] = useState("");
+
+  const handleUsernameChange = (e) => setUsername(e.target.value);
+
+  const userState = {
+    user: username,
+  };
+
+  return (
+    <div className="signin-page-container">
+      <div className="signin-content">
+        <h2 className="signin-heading">Sign in to us</h2>
+        <form className="signin-form" action="/home">
+          <div className="signin-form-group">
+            <label>Username or email address</label>
+            <input
+              className="signin-form-input"
+              type="text"
+              name="first_name"
+              required
+              onChange={handleUsernameChange}
+            />
+          </div>
+          <div className="signin-form-group">
+            <label>Password</label>
+            <Link className="signin-link" to="/forget-password">
+              <label className="right-label">Forgot password?</label>
+            </Link>
+            <input
+              className="signin-form-input"
+              type="password"
+              name="password"
+              required
+            />
+          </div>
+          <div className="signin-form-group">
+            <Link to="/welcome" state={userState}>
+              <button className="signin-button" id="sub_btn" type="button">
+                Login
+              </button>
+            </Link>
+          </div>
+        </form>
+        <footer className="signin-footer">
+          <p>
+            First time?{" "}
+            <Link className="signin-link" to="/">
+              Create an account
+            </Link>
+            .
+          </p>
+        </footer>
+      </div>
+    </div>
+  );
+}
